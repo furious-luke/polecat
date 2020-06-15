b6d7d5e3a6b40f79cb473f5495aea0a4da49fc9e
Luke Hodkinson <furious.luke@gmail.com>
2020-06-15 16:27:12 +1000

    Transition to Poetry

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '0.0.9'
+version = '0.0.10'
 
 setup(
     name='polecat',
@@ -24,32 +24,33 @@ setup(
     include_package_data=True,
     package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
     install_requires=[
-        'ujson==1.35',
-        'psycopg2-binary==2.8.4',
-        'graphql-core-next==1.0.2',
+        'ujson~=1.35',
+        'psycopg2-binary~=2.8.4',
+        'graphql-core-next~=1.0.2',
         # TODO: This should be in "cli".
-        'polecat-feedback==0.0.0',
+        'polecat-feedback',
         # 'graphql-server-core @ https://github.com/norman-thomas/graphql-server-core/tarball/master',
-        'pyjwt==1.7.1',
-        'pytimeparse==1.1.8',
-        'pytz==2019.1',
+        'pyjwt~=1.7.1',
+        'pytimeparse~=1.1.8',
+        'pytz~=2019.1',
         # Used to be in extras...
-        'click==7.0',
-        'halo==0.0.23',
-        'termcolor==1.1.0',
-        'cached-property==1.5.1'
+        'click~=7.0',
+        'halo~=0.0.29',
+        'termcolor~=1.1.0',
+        'cached-property~=1.5.1'
     ],
     extras_require={
         'cli': [
-            'pytest==4.3.1',
-            'pytest-asyncio==0.10.0',
-            'pytest-polecat==0.0.0',
-            'factory-boy==2.11.1',
-            'sanic==19.3.1',
-            'sanic-cors==0.9.9.post3',
-            'boto3==1.9.125',
-            'coolname==1.1.0',
-            'python-Levenshtein==0.12.0'
+            'pytest~=4.3.1',
+            'pytest-asyncio~=0.10.0',
+            'pytest-cov~=2.8.1',
+            'pytest-polecat',
+            'factory-boy~=2.11.1',
+            'sanic~=19.3.1',
+            'sanic-cors~=0.9.9.post3',
+            'boto3~=1.9.125',
+            'coolname~=1.1.0',
+            'python-Levenshtein~=0.12.0'
         ]
     },
     entry_points={
